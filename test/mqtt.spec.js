efb3bffc4cba2dc33f43c67db66229720c0f14db
Michael Woods <michael.woods@highlyrefined.com>
2020-12-28 14:05:20 -0500

    add payloads for binary_sensor support

--- test/mqtt.spec.js
+++ test/mqtt.spec.js
@@ -72,6 +72,8 @@ describe('MQTT', () => {
                     name: 'Ambient Air Temperature',
                     payload_available: 'true',
                     payload_not_available: 'false',
+                    payload_off: 'false',
+                    payload_on: 'true',
                     state_topic: 'homeassistant/sensor/XXX/ambient_air_temperature/state',
                     unit_of_measurement: '°C',
                     value_template: '{{ value_json.ambient_air_temperature }}'
@@ -102,6 +104,8 @@ describe('MQTT', () => {
                     name: 'Priority Charge Indicator',
                     payload_available: 'true',
                     payload_not_available: 'false',
+                    payload_off: 'false',
+                    payload_on: 'true',
                     state_topic: 'homeassistant/binary_sensor/XXX/ev_charge_state/state',
                     unit_of_measurement: undefined,
                     value_template: '{{ value_json.priority_charge_indicator }}'
@@ -134,6 +138,8 @@ describe('MQTT', () => {
                     name: 'Tire Pressure: Left Front',
                     payload_available: 'true',
                     payload_not_available: 'false',
+                    payload_off: 'false',
+                    payload_on: 'true',
                     state_topic: 'homeassistant/sensor/XXX/tire_pressure/state',
                     unit_of_measurement: 'kPa',
                     value_template: '{{ value_json.tire_pressure_lf }}'
